9eeccbea901b76e9726ebca6ec815e0a00b0e436
Sananthu-47 <sananthu47@gmail.com>
2020-12-25 17:28:11 +0530

    Show notification when we reply to a comment

--- includes/notification.php
+++ includes/notification.php
@@ -215,7 +215,44 @@ $total_notification = mysqli_num_rows($result);
                             $output.="<div class='post-notification-image my-2 col-2 p-0'><a href='view-all-comment.php?post_id=$post_id'><img src='assets/images/posts/$post_image'alt='image'></a></div>";
                         }
                         $output.= "</li>";
-                }
+                    }else
+                    if($row['type'] === 'comment_reply')
+                    {
+                            $reply_from = getUserInfoById("username",$user_id);
+                            $commented_at = $row['notified_at'];
+                            $comment_id = $row['comment_id'];
+                            $comment_from_me = getCommentInfo('comment',$comment_id);
+                            $comment_replied = new DateTime($commented_at);
+                            $interval = $comment_replied->diff($end_date);
+                            $time_message = getDateFormat($interval);
+                            $comment_message = $row['comment_message'];
+                            if($time_message !== 'Just now')
+                            {
+                            $time_array = (explode(" ",$time_message));
+                            $time_message = $time_array[0] . ' ' . $time_array[1][0];
+                            }
+                            $output .= "<li class='list-group-item d-flex align-items-center p-0";
+                            if($notification_status == "unseen")
+                            {
+                                $output.=" bg-not-seen";
+                            }else{
+                                $output.=" bg-light";
+                            }
+                            $output.= "'><div class='my-2 col-2 p-1'><div class='notification-user-preview'><a href='$reply_from'><img src='assets/images/profiles/$user_image'alt='image'></a></div></div>";
+                            $output.="<div class='d-flex flex-column col-8'><a href='view-all-comment.php?post_id=$post_id'><span class='text-";
+                            if($reply_from === $username)
+                            {
+                                $output.="dark'> You";
+                            }else{
+                                $output.="primary'>".$reply_from." ";
+                            }
+                            $output.="</span><span class='text-dark'> replied to your comment '$comment_from_me' as '$comment_message' <sub class='mx-2 text-secondary notification-time text-nowrap'>$time_message</sub></span></a></div>";
+                            if($post_image !== 'none')
+                            {
+                                $output.="<div class='post-notification-image my-2 col-2 p-0'><a href='view-all-comment.php?post_id=$post_id'><img src='assets/images/posts/$post_image'alt='image'></a></div>";
+                            }
+                            $output.= "</li>";
+                    }
         }
     $output.="<div class='text-center m-2' id='loaded-notification'>
     <div class='btn btn-info p-1 my-1 mx-auto' id='load-more-notifications' data-page='$page' data-id='$logged_in_user_id'>Load more</div>
